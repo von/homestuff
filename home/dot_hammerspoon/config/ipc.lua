1f9d444d24146278952a77245b7f865a47d980ec
Von Welch <von@vwelch.com>
2022-07-22 10:08:33 -0400

    ipc.lua: install ipc client in /opt/homebrew/ if it exists
    Instead of /usr/local.

--- home/dot_hammerspoon/config/ipc.lua
+++ home/dot_hammerspoon/config/ipc.lua
@@ -4,12 +4,13 @@ require("hs.ipc")
 
 local logger = hs.logger.new("ipc", "info")
 
-local myConfig = MyConfig["ipc"] or {}
--- XXX /usr/local seems to be hard-corded into the client
--- See https://github.com/Hammerspoon/hammerspoon/issues/3088
--- On Macs with Apple chips, with homebrew in /opt/homebrew, /usr/local
--- make not be writable.
-local path = myConfig["path"] or "/usr/local"
+local path = "/usr/local"
+
+-- On Macs with Apple chips, install cli in /opt/homebrew/ with Homebrew
+-- as /usr/local doesn't seem to be writable.
+if hs.fs.attributes("/opt/homebrew/") then
+  path = "/opt/homebrew/"
+end
 
 -- Second parameter to cliStatus() and cliInstall() is silent
 if hs.ipc.cliStatus(path, false) then
