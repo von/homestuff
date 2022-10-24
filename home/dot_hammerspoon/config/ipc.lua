c1a72a0d75fc0be09f07b89332f58614e2ef7803
Von Welch <von@vwelch.com>
2022-10-23 21:05:55 -0400

    ipc.lua: use homebrew_prefix from init.lua

--- home/dot_hammerspoon/config/ipc.lua
+++ home/dot_hammerspoon/config/ipc.lua
@@ -1,16 +1,11 @@
--- Make sure commandline client (/usr/local/bin/hs) is installed
+-- Make sure commandline client (${HOMEBREW_PREFIX}/bin/hs) is installed
 -- and IPC module is running.
 require("hs.ipc")
 
 local logger = hs.logger.new("ipc", "info")
 
-local path = "/usr/local"
-
--- On Macs with Apple chips, install cli in /opt/homebrew/ with Homebrew
--- as /usr/local doesn't seem to be writable.
-if hs.fs.attributes("/opt/homebrew/") then
-  path = "/opt/homebrew/"
-end
+-- Set in ../init.lua
+local path = homebrew_prefix
 
 -- Second parameter to cliStatus() and cliInstall() is silent
 if hs.ipc.cliStatus(path, false) then
