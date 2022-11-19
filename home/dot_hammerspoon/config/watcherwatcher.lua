1747f62640237f19894ba6cca330cdd256be4ab4
Von Welch <von@vwelch.com>
2022-11-19 11:22:25 -0500

    Changes for 0.5 version of WatcherWatcher

--- home/dot_hammerspoon/config/watcherwatcher.lua
+++ home/dot_hammerspoon/config/watcherwatcher.lua
@@ -1,18 +1,7 @@
 -- WatcherWatcher configuration
 local ww = hs.loadSpoon("WatcherWatcher")
 ww:debug(true)
-
-local flasher = ww.Flasher:new()
-flasher:debug(true)
-local startCallback, stopCallback = flasher:callbacks()
-ww.callbacks.cameraInUse = startCallback
-ww.callbacks.cameraNotInUse = stopCallback
-
-local menubar = ww.Menubar
-menubar:debug(true)
-menubar.monitorCameras = false
-startCallback, stopCallback = menubar:callbacks()
-ww.callbacks.micInUse = startCallback
-ww.callbacks.micNotInUse = stopCallback
+ww.Flasher:debug(true)
+ww.Menubar:debug(true)
 
 ww:start()
