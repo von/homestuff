9072efc0a536fd743f8c31743de5d6c48c1686b6
Von Welch <von@vwelch.com>
2022-12-09 12:35:23 -0500

    python.zsh: added pip-dev-install()

--- home/dot_zsh.d/python.zsh
+++ home/dot_zsh.d/python.zsh
@@ -6,3 +6,10 @@
 if test -e "$HOME/.pythonrc" ; then
   export PYTHONSTARTUP="$HOME/.pythonrc"
 fi
+
+# Install package in development mode
+# https://setuptools.pypa.io/en/latest/userguide/development_mode.html
+function pip-dev-install()
+{
+  pip install --editable .
+}
