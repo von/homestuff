ed2c7ba9d25b86d47386f59a06441b11fbb3487a
Von Welch <von@vwelch.com>
2024-02-29 17:40:40 -0500

    fzf_downloads_widget.plugin.zsh: use 'fzf-tmux' when appropriate

--- home/dot_zsh.d/plugins/fzf_downloads_widget/fzf_downloads_widget.plugin.zsh
+++ home/dot_zsh.d/plugins/fzf_downloads_widget/fzf_downloads_widget.plugin.zsh
@@ -3,14 +3,28 @@
 
 _fzf_download_widget() {
   local selected
+  local fzfcmd
+  local fzfopts
+
+  # fzf command to use
+  if test -n "$TMUX" ; then
+    fzfcmd="fzf-tmux"
+    # popup window at bottom of pane and fill pane
+    fzfopts="-x P -y P -w $(tmux display -p '#{pane_width}') -h $(tmux display -p '#{pane_height}')"
+  else
+    fzfcmd="fzf"
+  fi
+
   # Combination of '-d' and '--with-nth=-1' prunes path from displayed files
-  selected=$(\ls -1t ~/Downloads/* | \
-    FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS" \
-    fzf -d / --with-nth=-1 --preview="ls -l {}" --preview-window=top:2 )
+  selected=$(\ls -1t  ~/Downloads/* | \
+    FZF_DEFAULT_OPTS='--scheme=path -d / --with-nth=-1 --preview="ls -l {}" --preview-window=top:2' \
+    ${fzfcmd} ${fzfopts-} )
+  local ret=$?
   if test -n "${selected}" ; then
     # Quote and clean up path
     LBUFFER=${LBUFFER}${(q)selected:a}
     zle reset-prompt
   fi
+  return $ret
 }
 zle -N fzf-download-widget _fzf_download_widget
