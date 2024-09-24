b53ab7a956acd52b917241fcccf60a7fe1287313
Von Welch <von@vwelch.com>
2024-09-24 15:10:09 -0400

    home/dot_zsh.d/jump.zsh: handle tilde in jump target path

--- home/dot_zsh.d/jump.zsh
+++ home/dot_zsh.d/jump.zsh
@@ -17,6 +17,8 @@ function jump() {
     # Run fzf to allow the user to select one.
     selected=$(${HOME}/.bin/jumplist.sh -f ${(z)1:+-- --query=${1}})
     test $? -ne 0 && return $?
+    # Expand tilde
+    selected=${selected:s/~/${HOME}/}
   fi
   test -z "${selected}" && return 0
   cd ${selected}
