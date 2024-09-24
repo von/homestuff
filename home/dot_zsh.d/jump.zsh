96efdde1df1de47e77a4167faecefd1f8c860e83
Von Welch <von@vwelch.com>
2024-09-24 14:58:46 -0400

    home/dot_zsh.d/jump.zsh: just cd if single argument given
    And it's a legit directory.

--- home/dot_zsh.d/jump.zsh
+++ home/dot_zsh.d/jump.zsh
@@ -6,8 +6,18 @@
 #  $JUMP_ROOT - start of find for directories if set, otherwise $HOME
 
 function jump() {
-  local selected=$(${HOME}/.bin/jumplist.sh -f ${(z)1:+-- --query=${1}})
-  test $? -ne 0 && return $?
+  local selected=""
+  if test -n "${1}" -a -d "${1}" ; then
+    # If we were passed an actual path, then we have our target
+    # This will happen if the path was completed on the commandline
+    # when we were called.
+    selected="${1}"
+  else
+    # We were not passed a path or at least not a complete/valid one
+    # Run fzf to allow the user to select one.
+    selected=$(${HOME}/.bin/jumplist.sh -f ${(z)1:+-- --query=${1}})
+    test $? -ne 0 && return $?
+  fi
   test -z "${selected}" && return 0
   cd ${selected}
 }
