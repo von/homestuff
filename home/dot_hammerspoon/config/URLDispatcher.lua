1bd91dd1a3cd3a83ee7a66d91f92d84a9814e49d
Von Welch <von@vwelch.com>
2025-01-18 12:44:12 -0500

    URLDispatcher.lua: handle URLDispatcher not being loaded gracefully

--- home/dot_hammerspoon/config/URLDispatcher.lua
+++ home/dot_hammerspoon/config/URLDispatcher.lua
@@ -2,49 +2,51 @@
 -- See https://www.hammerspoon.org/Spoons/URLDispatcher.html
 -- Kudos: https://github.com/zzamboni/dot-hammerspoon
 
-local Zoom = require("Zoom")
-local myConfig = MyConfig["URLDispatcher"] or {}
+if spoon.URLDispatcher then
+  local Zoom = require("Zoom")
+  local myConfig = MyConfig["URLDispatcher"] or {}
 
-if myConfig['debugLevel'] then
-  spoon.URLDispatcher.logger.setLogLevel(myConfig['debugLevel'])
-end
-
-local noApp = nil  -- for readability
-
-spoon.URLDispatcher.url_patterns = {
-  -- j == join, s == start
-  -- Don't redirect Zoom webinar URLs ('w') as Zoom.handleURL() doesn't
-  -- handle them.
-  { "https://.*.zoom.us/[js]/", noApp, Zoom.handleURL },
-  { "https://.*.zoomgov.com/[js]/", noApp, Zoom.handleURL },
-  { "msteams:", "com.microsoft.teams" }
-}
-
-if myConfig["useURLOpener"] then
-  -- Don't use hs.urlevent.open() as that will result in an
-  -- infinite loop calling URLDispatcher
-  local URLOpener = require("URLOpener")
-  URLOpener.setDefaultPersona(defaultChromePersona)
-  table.insert(spoon.URLDispatcher.url_patterns, { ".*", noApp, URLOpener.open })
-else
-  --Use chrome.openURL()
-  local chrome = require("chrome")
-  local function opener(url)
-    local args={url=url, persona=defaultChromePersona}
-    chrome.openURL(args)
+  if myConfig['debugLevel'] then
+    spoon.URLDispatcher.logger.setLogLevel(myConfig['debugLevel'])
   end
-  table.insert(spoon.URLDispatcher.url_patterns, { ".*", noApp, opener })
-end
 
-if myConfig["defaultHandler"] then
-  spoon.URLDispatcher.default_handler = myConfig["defaultHandler"]
-end
+  local noApp = nil  -- for readability
 
-spoon.URLDispatcher.url_redir_decoders = {}
+  spoon.URLDispatcher.url_patterns = {
+    -- j == join, s == start
+    -- Don't redirect Zoom webinar URLs ('w') as Zoom.handleURL() doesn't
+    -- handle them.
+    { "https://.*.zoom.us/[js]/", noApp, Zoom.handleURL },
+    { "https://.*.zoomgov.com/[js]/", noApp, Zoom.handleURL },
+    { "msteams:", "com.microsoft.teams" }
+  }
 
-if myConfig["redirectTeams"] then
-  -- Send MS Teams URLs directly to the app
-  -- Kudos: https://github.com/zzamboni/dot-hammerspoon/blob/master/init.org
-  table.insert(spoon.URLDispatcher.url_redir_decoders,
-    { "MS Teams URLs", "(https://teams.microsoft.com.*)", "msteams:%1", true })
+  if myConfig["useURLOpener"] then
+    -- Don't use hs.urlevent.open() as that will result in an
+    -- infinite loop calling URLDispatcher
+    local URLOpener = require("URLOpener")
+    URLOpener.setDefaultPersona(defaultChromePersona)
+    table.insert(spoon.URLDispatcher.url_patterns, { ".*", noApp, URLOpener.open })
+  else
+    --Use chrome.openURL()
+    local chrome = require("chrome")
+    local function opener(url)
+      local args={url=url, persona=defaultChromePersona}
+      chrome.openURL(args)
+    end
+    table.insert(spoon.URLDispatcher.url_patterns, { ".*", noApp, opener })
+  end
+
+  if myConfig["defaultHandler"] then
+    spoon.URLDispatcher.default_handler = myConfig["defaultHandler"]
+  end
+
+  spoon.URLDispatcher.url_redir_decoders = {}
+
+  if myConfig["redirectTeams"] then
+    -- Send MS Teams URLs directly to the app
+    -- Kudos: https://github.com/zzamboni/dot-hammerspoon/blob/master/init.org
+    table.insert(spoon.URLDispatcher.url_redir_decoders,
+      { "MS Teams URLs", "(https://teams.microsoft.com.*)", "msteams:%1", true })
+  end
 end
