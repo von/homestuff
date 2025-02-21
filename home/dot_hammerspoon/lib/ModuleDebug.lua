6ea0c0db6add615f4862ccdb1aab5f9d1b5bf742
Von Welch <von@vwelch.com>
2025-02-21 13:03:43 -0500

    ModuleDebug.lua: added support for Spoons

--- home/dot_hammerspoon/lib/ModuleDebug.lua
+++ home/dot_hammerspoon/lib/ModuleDebug.lua
@@ -18,10 +18,11 @@ function Module:debug(enable)
   end
 end
 
--- Should be a table of <module name>,<value>
-Module.modules = MyConfig["debug.modules"] or {}
 
 function Module:start()
+  -- Handle debug.modules, loadded via require()
+  -- Should be a table of <module name>,<value>
+  Module.modules = MyConfig["debug.modules"] or {}
   if Module.modules then
     for module,v in pairs(Module.modules) do
       if v then
@@ -47,6 +48,39 @@ function Module:start()
         if not result then
           Module.log.df("Error enabling debugging for %s: %s",
             module, errormsg)
+        end
+      end
+    end
+  end
+
+  -- Handle debug.spoons, loadded via hs.loadded()
+  -- Should be a table of <module name>,<value>
+  Module.spoons = MyConfig["debug.spoons"] or {}
+  if Module.spoons then
+    for spoon,v in pairs(Module.spoons) do
+      if v then
+        Module.log.df("Turning on debugging for spoon %s", spoon)
+        local f = function()
+          -- Try to call spoon:debug(true) if it has a debug() method
+          local s = hs.loadSpoon(spoon)
+          if s.debug then
+            Module.log.df("Calling %s:debug(true)", spoon)
+            s:debug(true)
+            return
+          end 
+          -- Try setting level on spoon.logger
+          if s.logger then
+            Module.log.df("Setting loglevel on %s.logger to debug", spoon)
+            -- Yes "." not ":" - it's a static function
+            s.logger.setLogLevel("debug")
+            return
+          end
+          Module.log.ef("Could not turn on debugging on %s", spoon)
+        end
+        local result, errormsg = pcall(f)
+        if not result then
+          Module.log.df("Error enabling debugging for %s: %s",
+            spoon, errormsg)
         end
       end
     end
