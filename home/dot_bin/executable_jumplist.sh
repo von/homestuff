7614c61c8618501d977181f108739152c3f3f4e3
Von Welch <von@vwelch.com>
2026-02-02 08:59:59 -0500

    Updated .jump_targets to be name/path pairs

--- home/dot_bin/executable_jumplist.sh
+++ home/dot_bin/executable_jumplist.sh
@@ -6,28 +6,42 @@
 #
 # The list comes from ~/.jump_targets and ~/.jump_targets_local
 
-cmd-list()
+# Format for cmd-list().
+#   %path will be converted into full path
+#   %name will be converted into key
+list_format="%path"
+
+# Read jump list from files into associate array JUMP_TARGETS
+_read_jump_list()
 {
   local tpath=${JUMP_TARGETS_PATH:-${HOME}/.jump_targets}
   local ltpath=${JUMP_TARGETS_LOCAL_PATH:-${HOME}/.jump_targets_local}
 
-  typeset -a JUMP_TARGETS
-  # https://zsh.sourceforge.io/Doc/Release/Zsh-Modules.html#The-zsh_002fmapfile-Module
-  zmodload zsh/mapfile
+  typeset -Ag JUMP_TARGETS=()
 
-  # Read jump_targets into JUMP_TARGETS, removing comments
-  # Kudos: https://stackoverflow.com/a/12652267/197789
-  JUMP_TARGETS=( "${(f)mapfile[$tpath]}" )
+  for file in ${tpath} ${ltpath} ; do
+    if test -f ${file} ; then
+      while read -r key value; do
+        if [[ ${key} == \#* ]] ; then
+          : # comment
+        else
+          JUMP_TARGETS[$key]="$value"
+        fi
+      done < ${file}
+    fi
+  done
 
-  if test -f "${ltpath}" ; then
-    JUMP_TARGETS+=( "${(f)mapfile[$ltpath]}" )
-  fi
+  # success. JUMP_TARGETS set.
+}
 
-  # Remove comments
-  # Kudos: https://stackoverflow.com/a/41876600/197789
-  JUMP_TARGETS=( ${JUMP_TARGETS:#\#*} )
+# List jumplist directories
+cmd-list()
+{
+  _read_jump_list  # Sets JUMP_TARGETS
 
-  printf '%s\n' "${JUMP_TARGETS[@]}"
+  for name path in ${(kv)JUMP_TARGETS}; do
+    echo ${list_format:gs/%name/$name/:gs/%path/$path/}
+  done
 
   return 0
 }
@@ -45,6 +59,7 @@ Usage: $0 [<options>] -- [<fzf options>]
 
 Options:
   -f              Select path with fzf and print [DEFAULT].
+  -F <fmt>        Use fmt for list (defaule: ${list_format}
   -h              Print help and exit.
   -l              Print jump list and exit.
   -T              Force 'fzf' instead of 'fzf-tmux'
@@ -65,9 +80,10 @@ else
   FZF_CMD="fzf"
 fi
 
-while getopts ":fhlT" opt; do
+while getopts ":fF:hlT" opt; do
   case $opt in
     f) CMD="cmd-fzf" ;;
+    F) list_format=${OPTARG} ;;
     h) usage ; exit 0 ;;
     l) CMD="cmd-list" ;;
     T) FZF_CMD="fzf" ;;
