a665e54c5a4d32f2816576edd9781c2839dd0c86
Von Welch <von@vwelch.com>
2026-02-08 20:47:33 -0500

    run_once_after_make_virtualenvs.sh: added check for 'make-virtualenvs'

--- home/run_once_after_make_virtualenvs.sh
+++ home/run_once_after_make_virtualenvs.sh
@@ -4,5 +4,10 @@
 #
 # make-virtualenvs:
 # https://github.com/von/scripts/blob/main/make-virtualenvs.py
-make-virtualenvs create || exit 1
+# May not be installed yet, so check.
+if (( $+commands[make-virtualenvs] )) ; then
+  make-virtualenvs create || exit 1
+else
+  echo "make-virtualenvs not found."
+fi
 exit 0
